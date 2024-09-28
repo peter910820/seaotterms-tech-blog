9aa26b1f56fa5a4b672b43167bb3923845ca3dd8
peter910820 <peter910820@gmail.com>
2024-09-28 22:06:21 +0800

    make index page navbar and background

--- seaotterms_tech_blog/article/views.py
+++ seaotterms_tech_blog/article/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from .models import Tag
 
+def index(request):
+    return render(request, 'index.html')
+
 def main_page(request):
     tags = Tag.objects.all()
     return render(request, 'main_page.html', {'tags': tags})
