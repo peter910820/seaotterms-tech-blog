76163c63d30659a031375816df3afe21f7a93af4
peter910820 <peter910820@gmail.com>
2024-09-29 16:33:29 +0800

    make front-end style

---
+++ seaotterms_tech_blog/static/js/animate.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", (event) => {
+    new cursoreffects.snowflakeCursor();
+});
+
+let typed = new Typed('#typed', {
+    strings: ['Welcome to, SeaotterMS Blog!'],
+    typeSpeed: 50,
+});
